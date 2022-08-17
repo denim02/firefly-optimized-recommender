cafe6b2b7a0e756787994d75f175b63a6ad478ad
Deni Mastori <denimastori02@gmail.com>
2022-08-17 15:12:29 +0200

    No changes

--- src/main.py
+++ src/main.py
@@ -28,31 +28,26 @@ ml, data, rankings = loadMovieLensData()
 # tuned_SVD_params = fa.solve()
 
 tuned_SVD_params = {
-    'reg_all': 0.055446287453986845,
-    'n_factors': 86,
-    'n_epochs': 89,
-    'lr_all': 0.0022187603665117447
+    'reg_all': 0.002987,
+    'n_factors': 93,
+    'n_epochs': 87,
+    'lr_all': 0.002987
 }
 
 # Build evaluation object
 eval = Evaluator(data,rankings)
 
-print("\nRunning evaluation between tuned SVD model, untuned SVD, KNN, and Normal Predictor...\n")
+print("\nRunning evaluation between tuned SVD model, untuned SVD, and Normal Predictor...\n")
 
 # Build tuned SVD, untuned SVD, random models
 svdtuned = SVD(reg_all=float(tuned_SVD_params['reg_all']), n_factors=int(tuned_SVD_params['n_factors']), n_epochs=int(tuned_SVD_params['n_epochs']), lr_all=float(tuned_SVD_params['lr_all']))
-knn = KNNBasic()
 svd = SVD()
 random = NormalPredictor()
 
 # Add models to evaluation object
 eval.addModel(svdtuned,'SVDtuned')
 eval.addModel(svd,'SVD')
-eval.addModel(knn,'KNN Basic')
 eval.addModel(random,'Random')
 
 # Evaluate object = fit models, build topN lists, run prediction/hitrate based metrics
 eval.evaluateModel(True)
-
-# Build topN list for target user 56
-eval.sampleUser(ml)
